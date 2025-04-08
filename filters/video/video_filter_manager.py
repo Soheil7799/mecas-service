b1fed21c40f712eee32ca149f500a95d950624e5
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-08 17:41:57 +0200

    Add filters file structure

---
+++ filters/video/video_filter_manager.py
@@ -0,0 +1,16 @@
+class VideoFilterManager:
+    def _grayScale(self):
+        #TODO : here grayscaling will happen
+        return ""
+    def _colorInvert(self):
+        #TODO : here color inverting will happen
+        return ""
+    def _frameIntp(self):
+        #TODO : here frame interpolation will happen
+        return ""
+    def _upscaling(self):
+        #TODO : here resolution upscaling will happen
+        return ""
+    def apply_filters(self):
+        #TODO : here all the needed filters will happen
+        return ""
