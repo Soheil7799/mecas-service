b9c530f44a446f02cb0344afbe070d5b4c7f6bda
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-09 15:48:49 +0200

    Update gain compression filter
    added ability to cutoff above limiter, fix final sample

--- filters/audio/gain_compression.py
+++ filters/audio/gain_compression.py
@@ -1,10 +1,11 @@
 import numpy as np
 import scipy.io.wavfile as wav
+import logging
 # to convert decibels
 def db_to_linear(db_value):
     return 10 ** (db_value / 20.0)
 
-def apply_gain_compression(input_file = "input.wav", output_file="output.wav", compressor_threshold_db=-1, limiter_threshold_db=0 , compressor_ratio=4 , limiter_ratio=10):
+def apply_gain_compression(input_file = "../../files/input/input.wav", output_file="../../files/temp/output.wav", compressor_threshold_db=-1, limiter_threshold_db=0 , compressor_ratio=4 , limiter_ratio=10):
 
     compressor_threshold = db_to_linear(compressor_threshold_db)
     limiter_threshold = db_to_linear(limiter_threshold_db)
@@ -29,11 +30,16 @@ def apply_gain_compression(input_file = "input.wav", output_file="output.wav", c
     mask_limit = abs_sample > limiter_threshold
     # like a loop, if any need compressing, it will do it
     if np.any(mask_limit):
-        abs_sample[mask_limit] = limiter_threshold + (abs_sample[mask_limit] - limiter_threshold) / limiter_ratio
+        if limiter_ratio == 0 :
+            abs_sample[mask_limit] = limiter_threshold
+        else:
+            abs_sample[mask_limit] = limiter_threshold + (abs_sample[mask_limit] - limiter_threshold) / limiter_ratio
 
     final_sample = signs * abs_sample
+    final_sample_int = (final_sample * 32767.0).astype(np.int16)
+    wav.write(output_file,sample_rate,final_sample_int)
 
-    print (f"{sample_int}\n{final_sample}\n{sample_int - final_sample}")
-
+    print (f"{sample_int}\n{final_sample_int}\n{sample_int - final_sample_int}")
+    logging.info("Gain Compressed file has been made")
 
 apply_gain_compression()
