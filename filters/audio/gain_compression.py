632cce5247b0478f5e5332269d7e13714efd2366
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-09 13:11:56 +0200

    Update gain compression
    it is doing the compression

--- filters/audio/gain_compression.py
+++ filters/audio/gain_compression.py
@@ -4,7 +4,7 @@ import scipy.io.wavfile as wav
 def db_to_linear(db_value):
     return 10 ** (db_value / 20.0)
 
-def apply_gain_compression(input_file = "input.wav", output_file="output.wav", compressor_threshold_db=-3, limiter_threshold_db=1):
+def apply_gain_compression(input_file = "input.wav", output_file="output.wav", compressor_threshold_db=-1, limiter_threshold_db=0 , compressor_ratio=4 , limiter_ratio=10):
 
     compressor_threshold = db_to_linear(compressor_threshold_db)
     limiter_threshold = db_to_linear(limiter_threshold_db)
@@ -12,9 +12,28 @@ def apply_gain_compression(input_file = "input.wav", output_file="output.wav", c
     # Read the audio file
     sample_rate, sample_original = wav.read(input_file)
     # changing the format numpy int16
-    sample_int = sample_original.astype(np.int16)
-    
-    print (f"{sample_int} \n {sample_original}")
+    sample_int = sample_original.astype(np.int16) / 32767
+
+    # we take the signs and make it absolute so in the "if" sections it is easier for conditions
+    signs = np.sign(sample_int)
+    abs_sample = np.abs(sample_int)
+
+    # we find the values between the two thresholds to compress them (default ratio is 4 to 1 )
+    mask_compression = (abs_sample > compressor_threshold ) & (abs_sample < limiter_threshold )
+    # like a loop, if any need compressing, it we
+    if np.any(mask_compression):
+        abs_sample[mask_compression] = compressor_threshold + (abs_sample[mask_compression] - compressor_threshold) / compressor_ratio
+
+    # we find the values surpassing the limiter thresholds to cut/compress them (default ratio is 10 to 1 )
+    # any ratio above 10 is supposed to act like limiter
+    mask_limit = abs_sample > limiter_threshold
+    # like a loop, if any need compressing, it will do it
+    if np.any(mask_limit):
+        abs_sample[mask_limit] = limiter_threshold + (abs_sample[mask_limit] - limiter_threshold) / limiter_ratio
+
+    final_sample = signs * abs_sample
+
+    print (f"{sample_int}\n{final_sample}\n{sample_int - final_sample}")
 
 
 apply_gain_compression()
