eb813d2709a7edf0839a0739781780fdf3219ab1
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-09 10:10:07 +0200

    Fix Typo

--- api/routers/stream.py
+++ api/routers/stream.py
@@ -3,6 +3,6 @@ router = APIRouter()
 
 # To get the video for streaming
 @router.get("/{video_id}")
-async def analyze_file(video_id: str):
+async def stream(video_id: str):
     # TODO : make the processed video stream on webpage
     return ""
