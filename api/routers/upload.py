f80e16cabb2445928c38127c07046c51a014ba8e
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-10 20:41:35 +0200

    Update upload.py, successfully uploads file

--- api/routers/upload.py
+++ api/routers/upload.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 router = APIRouter()
-
+FILEPATH = "./files/input"
 # To upload a video into server
-@router.post("/", status_code=201)
-async def upload_video():
+@router.post("/uploadfile/", status_code=201)
+async def upload_video(video_file: UploadFile = File(...)):
     #TODO : get a video/audio file from user
-    return ""
+    file_type = video_file.content_type
+    file_path = f"{FILEPATH}/{video_file.filename}"
+    with open(file_path,"wb+") as file_object:
+        file_object.write(video_file.file.read())
+    return {"filename" : video_file.content_type}
 
 # To delete a previously uploaded file
 @router.delete("/{video_id}")
