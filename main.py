61bb857013eb6a0557784312431d15092ee1aa7b
Mostafa Shirvani <MostafaShirvani1377@outlook.com>
2025-04-15 12:01:21 +0200

    Add frontend with HTML,CSS,JS

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 import api.api as api
-# Declaring endpoints
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development only. In production, specify exact domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Declaring endpoints
 app.include_router(api.api_router)
-
-
-
-
-
-
